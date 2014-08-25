eaf03ee382d66168fc609847f3ce10ee8bf3fec6
ankoran <jrbushcraft@gmail.com>
2014-08-24 19:09:27 -0500

    finished layout and routes

--- Gemfile
+++ Gemfile
@@ -29,3 +29,10 @@ group :production do
   gem 'pg', '0.15.1'
   gem 'rails_12factor', '0.0.2'
 end
+
+
+#ruby-gemset=railstutorial_rails_4_0
+
+gem 'rails', '4.0.8'
+gem 'bootstrap-sass', '2.3.2.0'
+gem 'sprockets', '2.11.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
   get "static_pages/home"
   get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
@@ -57,8 +58,10 @@ SampleApp::Application.routes.draw do
   #   end
 end
 SampleApp::Application.routes.draw do
-	get "static_pages/home"
-	get "static_pages/help"
-	get "static_pages/about"
-	get "static_pages/contact"
+  get "users/new"
+  root  'static_pages#home'
+   match '/signup',  to: 'users#new',            via: 'get'
+   match '/help',    to: 'static_pages#help',    via: 'get'
+   match '/about',   to: 'static_pages#about',   via: 'get'
+   match '/contact', to: 'static_pages#contact', via: 'get'
 end
